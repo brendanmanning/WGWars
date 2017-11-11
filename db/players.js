2d4965e52369c428ef55c45698f0535aad40ff57
Brendan Manning <BrendanManning19380@gmail.com>
2017-11-10 22:56:21 -0500

    Added documentation

--- db/players.js
+++ db/players.js
@@ -7,12 +7,25 @@ async function getPlayers(connection) {
     });
 }
 
+/**
+ * Gets a player by their database id
+ * @param {int} id 
+ * @returns the player's database row as a JSON object
+ */
 async function getPlayer(id) {
     var database = await get_database_connection();
     var results = await database.query('select * from players WHERE id=? LIMIT 1', [id]);
     return results[0];
 }
 
+/**
+ * 
+ * @param {string} name This user's name (will be publically available)
+ * @param {string} email The email of the user (will not be publically available)
+ * @param {int} game The game id to attatch this new round to
+ * @param {string} coordinates The coordinates of where this player lives. "latitude,longitude"
+ * @returns The newly created player as a JSON object
+ */
 async function createPlayer(name, email, game, coordinates) {
     var database = await get_database_connection();
     var result = await database.query(
