5804d0c8f2ec2c3444ee919050f07f27061a22cf
Brendan Manning <BrendanManning19380@gmail.com>
2017-12-02 23:08:02 -0500

    Added database handlers

---
+++ db/assignments.js
@@ -0,0 +1,28 @@
+var get_database_connection = require('../db.js');
+
+/**
+ * Returns all the target assignments for a given round
+ * @param {int} round The round ID to find results for
+ * @return {[Object]} A JSON repreentation of the MySQL result 
+ */
+async function getAssignments(round) {
+    var database = await get_database_connection();
+    var results = await database.query("SELECT * FROM targets WHERE round=?", [round]);
+    return results;
+}
+
+/**
+ * Inserts a target assignment record
+ * @param {int} round The round ID for these target assignments
+ * @param {Object} assignment The target assignment object
+ * @returns {Object} The newly created target record as a JSON object
+ */
+async function createAssignment(round) {
+    var database = await get_database_connection();
+    var results = await database.query("INSERT INTO targets", [round]);
+    return results;
+}
+
+module.exports = {
+    getAssignments
+}
